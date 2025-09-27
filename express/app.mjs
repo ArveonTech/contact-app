63ce879531a90bf6af6de3bab8d473e9f41fcbff
Ahda-Rizqi <AhdaRizqi1199@gmail.com>
2025-09-27 19:33:46 +0700

    express.js contact-app #1

--- express/app.mjs
+++ express/app.mjs
@@ -1,29 +1,28 @@
 import express from "express";
-import morgan from "morgan";
 const app = express();
 const port = 3000;
 import expressLayouts from "express-ejs-layouts";
+import { contactsJSON, findContactsJSON } from "./utils/contact.js";
 
-// gunakan ejs
+// gunakan ejs-layout
 app.set("view engine", "ejs");
 
 // third-party middleware
 app.use(expressLayouts);
-app.use(morgan("dev"));
 
 // built-in middleware
 app.use(express.static("public"));
 
 // application level middleware
-app.use((req, res, next) => {
-  console.log(`Time : ${Date.now()}`);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(`Time : ${Date.now()}`);
+//   next();
+// });
 
-app.use((req, res, next) => {
-  console.log(`ini middleware ke-2`);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(`ini middleware ke-2`);
+//   next();
+// });
 
 app.get("/", (req, res) => {
   // res.send("Hello World!");
@@ -58,12 +57,14 @@ app.get("/about", (req, res) => {
   res.render("about", { layout: "layouts/main-layouts", title: "Halaman About" });
 });
 
-app.get("/contact", (req, res) => {
-  res.render("contact", { layout: "layouts/main-layouts", title: "Halaman Contact" });
+app.get("/contact", async (req, res) => {
+  const contacts = await contactsJSON();
+  res.render("contact", { layout: "layouts/main-layouts", title: "Halaman Contact", contacts });
 });
 
-app.get("/product/:id", (req, res) => {
-  res.send(`Proudct ID : ${req.params.id} <br> Category ID : ${req.query.category}`);
+app.get("/contact/:nama", async (req, res) => {
+  const contact = await findContactsJSON(req.params.nama);
+  res.render("detail", { layout: "layouts/main-layouts", title: "Detail Contact", contact });
 });
 
 // middleware = apapun route yg didapat akan menjalankan ini, letak di bawah/jika tidak ada halaman misal
